b1ce05daba4a236c1a224b1d9d4742d8890cfdf9
sgnilo <43700959+sgnilo@users.noreply.github.com>
2019-05-31 16:56:41 +0800

    Update and rename main/main.js to  main/main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,15 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    let waitPrice=0.25*inputs.parkTime
+    let instancePrice=0
+    if(inputs.distance<=2){
+    	instancePrice=6
+    }
+    else if(inputs.distance>2&&inputs.distance<=8){
+    	instancePrice=6+(inputs.distance-2)*0.8
+    }
+    else{
+    	instancePrice=6+6*0.8+(inputs.distance-8)*(0.8+0.8*0.5)
+    }
+    return Math.round(waitPrice+instancePrice);
 };
